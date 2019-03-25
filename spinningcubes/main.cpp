0a27ddedaf499d1dbb3a4f58058ca307c4daf4ae
Nils Gibson <blueeffusion@gmail.com>
2019-03-25 09:51:16 -0500

    Update main.cpp

--- spinningcubes/main.cpp
+++ spinningcubes/main.cpp
@@ -214,7 +214,7 @@ int main()
 		projection = glm::perspective(glm::radians(35.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		// pass transformation matrices to the shader
-		ourShader.setMat4("projection", projection); // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+		ourShader.setMat4("projection", projection); // this is the brutish way, setting the projection matrix each frame.
 		ourShader.setMat4("view", view);
 
 		// render boxes
