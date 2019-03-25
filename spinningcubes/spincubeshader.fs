4df90b16fe37dfa37522855be9eb4db05be422ab
Nils Gibson <blueeffusion@gmail.com>
2019-03-24 22:56:28 -0500

    Update spincubeshader.fs

--- spinningcubes/spincubeshader.fs
+++ spinningcubes/spincubeshader.fs
@@ -9,6 +9,6 @@ uniform sampler2D texture2;
 
 void main()
 {
-    // linearly interpolate between both textures (80% container, 20% awesomeface)
+    // linearly interpolate between both textures (80% containertex, 20% wojak)
     FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
